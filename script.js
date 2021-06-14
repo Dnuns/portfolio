e55a89de9d120cdee9483af4336cb7bf842c3858
David Nunes <51808846+Dnuns@users.noreply.github.com>
2021-06-14 18:32:26 -0100

    add type effect

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ menuMobile.addEventListener('click', () => {
     const nav = document.getElementsByClassName('menu-items')[0];
     nav.classList.toggle('active');
 });
+
+
+//typing effect
+
+let i = 0, welcomeText;
+
+welcomeText = "I'm a junior full stack developer" ;
+
+function typing(){   
+
+   
+
+    if(i < welcomeText.length){
+
+        document.getElementsByClassName('wel-cont-txt')[0].innerHTML += welcomeText.charAt(i);
+        
+        i++;
+    }
+
+    setTimeout(typing,50);
+
+}
+
+typing();
